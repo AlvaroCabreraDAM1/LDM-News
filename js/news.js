9647064e79583ba21dc49b7f1a7316d3535db894
Alvaro Cabrera Fernandez <32370797+AlvaroCabreraDAM1@users.noreply.github.com>
2018-05-23 14:02:50 +0200

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -1,6 +1,7 @@
 var loaded = 1;
 var toggleOn = false;
 var theme = 0;
+var manectricX = 100;
 
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height() && toggleOn == true) {
@@ -16,6 +17,9 @@ function load() {
 			$("#loading").hide();
         }); 
 		loaded++;
+		if (loaded > 2) {
+			$("#loadButton").hide();
+		}
     }
 };
 
@@ -44,6 +48,15 @@ function addrow(json) {
     })
 };
 
+function manectric() {
+  $("#manectric").css("margin-left",  manectricX + "%");
+  if (manectricX < -50) {
+	  manectricX = 100
+  } else {
+	manectricX--; 
+	}
+};
+
 $(document).ready(function(){
 	
 	$("#toggleOn").tooltip();
@@ -65,26 +78,14 @@ $(document).ready(function(){
 		}
     });
 	
-	$("#themeButton").click(function() {
-		switch (theme) {
-			
-			case 0:
-			theme = 1;
-			break;
-			
-			case 1:
-			theme = 0;
-			break;
-			
-		}
-		
-	});
 	
 	$("#loadButton").click(function() {load();});
-	
+		
 	$("body").on("click", ".newImg", function(){
 		var id = $(this).attr('id');
 		$("#modalImg").attr("src","img/news/new" + id + "Big.png");
     });
+	
+	interval = setInterval(manectric, 100); 
 	
 });
